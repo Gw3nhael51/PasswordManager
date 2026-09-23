a32e99ff9c6656a171ea1aef3b4abd525395766f
Gw3nhael51 <ltgwenhael2003@gmail.com>
2026-09-23 10:59:57 +0200

    feat: ajout de la table vault_items

--- app/database/init.sql
+++ app/database/init.sql
@@ -12,10 +12,24 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- insertion des utilisateurs de test
+-- insertion des utilisateurs de test par defaut
 -- admin / securepass
 -- test_user / 123456
 
 INSERT INTO users (username, password, role) VALUES
     ('admin', '$2y$12$z.Bp2f86Gt7IKLyhwP0XPe/TuX1yma3gUZA6VB9zoBhJPvW3Uf34S', 'admin'),
     ('test_user', '$2y$12$.bFMV9RhLE5wEiySFeyXmuXtTbgWCba12dGHVSAuACS.ddbpz8JyG', 'user');
+
+-- Definir table du coffre avec un proprietaire et partage
+CREATE TABLE vault_items (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(100) NOT NULL,
+    website_url VARCHAR(255) NOT NULL,
+    encrypted_password TEXT NOT NULL,
+    iv VARCHAR(50) DEFAULT 'Général',
+    
+    -- vu par tous le sutilisateurs
+    is_shared BOOLEAN DEFAULT FALSE, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
